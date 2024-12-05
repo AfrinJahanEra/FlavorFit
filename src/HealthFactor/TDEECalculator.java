ec7c9c8dea840a69dd223e3dacd9bcaff36db0ff
era97 <afrinjahanera03@gmail.com>
2024-12-05 13:20:44 +0600

    TDEECalculator class added

---
+++ src/HealthFactor/TDEECalculator.java
@@ -0,0 +1,16 @@
+package src.HealthFactor;
+
+public class TDEECalculator implements HealthMetricCalculator {
+    private final double bmr;
+    private final double activityLevel; // Multiplier based on activity (e.g., sedentary = 1.2, moderate = 1.55, active = 1.9)
+
+    public TDEECalculator(double bmr, double activityLevel) {
+        this.bmr = bmr;
+        this.activityLevel = activityLevel;
+    }
+
+    @Override
+    public double calculate() {
+        return bmr * activityLevel;
+    }
+}
