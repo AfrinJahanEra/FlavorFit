a713da7cc8efc4114a76661411dc4be6a565e0c7
era97 <afrinjahanera03@gmail.com>
2024-12-05 13:19:32 +0600

    HeartRateCalculator class added

---
+++ src/HealthFactor/HeartRateCalculator.java
@@ -0,0 +1,17 @@
+package src.HealthFactor;
+
+public class HeartRateCalculator implements HealthMetricCalculator {
+    private final int age;
+    private final double restingHeartRate;
+
+    public HeartRateCalculator(int age, double restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+
+    @Override
+    public double calculate() {
+        double maxHeartRate = 220 - age;
+        return 0.7 * (maxHeartRate - restingHeartRate) + restingHeartRate; // Target heart rate
+    }
+}
