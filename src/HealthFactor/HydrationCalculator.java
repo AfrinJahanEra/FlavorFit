4b0ebee9b32d03a3e0b6a609b3e9cdfbb48ce9f3
era97 <afrinjahanera03@gmail.com>
2024-12-05 13:17:00 +0600

    HydrationCalculator class added

---
+++ src/HealthFactor/HydrationCalculator.java
@@ -0,0 +1,14 @@
+package src.HealthFactor;
+
+public class HydrationCalculator implements HealthMetricCalculator {
+    private final double weightInKg;
+
+    public HydrationCalculator(double weightInKg) {
+        this.weightInKg = weightInKg;
+    }
+
+    @Override
+    public double calculate() {
+        return weightInKg * 0.033; // Water intake in liters
+    }
+}
