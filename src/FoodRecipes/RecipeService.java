dcce931af9eb869ef9c715e54fae6ef72d2225ca
era97 <afrinjahanera03@gmail.com>
2024-12-06 18:33:02 +0600

    recipes service added'

--- src/FoodRecipes/RecipeService.java
+++ src/FoodRecipes/RecipeService.java
@@ -1,3 +1,25 @@
 package src.FoodRecipes;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class RecipeService {
+    private final RecipeRepository repository;
+
+    public RecipeService(RecipeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Recipe> getRecipesByGoal(String goal) {
+        return repository.loadRecipes().stream()
+                .filter(recipe -> recipe.getGoal().equalsIgnoreCase(goal))
+                .collect(Collectors.toList());
+    }
+
+    public List<Recipe> getRecipesByIngredient(String ingredient) {
+        return repository.loadRecipes().stream()
+                .filter(recipe -> recipe.getIngredients().stream()
+                        .anyMatch(i -> i.toLowerCase().contains(ingredient.toLowerCase())))
+                .collect(Collectors.toList());
+    }
+}
