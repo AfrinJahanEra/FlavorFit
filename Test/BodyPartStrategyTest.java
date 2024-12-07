6cbab7d9fc36cf999510d35d99be4a4297a7f5f7
era97 <afrinjahanera03@gmail.com>
2024-12-07 13:16:24 +0600

    BodyPart Code Test

--- Test/BodyPartStrategyTest.java
+++ Test/BodyPartStrategyTest.java
@@ -1,1 +1,22 @@
 package Test;
+
+import org.junit.jupiter.api.Test;
+
+import src.Exercise.BodyPartStrategy;
+import src.Exercise.Exercise;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BodyPartStrategyTest {
+
+    @Test
+    public void testLegExercises() {
+        BodyPartStrategy strategy = new BodyPartStrategy();
+        List<Exercise> exercises = strategy.getExercises("leg");
+        assertEquals(2, exercises.size());
+        assertEquals("Squats", exercises.get(0).getName());
+        assertEquals("Lunges", exercises.get(1).getName());
+    }
+}
