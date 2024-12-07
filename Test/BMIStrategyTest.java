0fdb89bda3ecaea20c77d280beaa06e43422fbc3
era97 <afrinjahanera03@gmail.com>
2024-12-07 13:12:03 +0600

    work in BMIS test

--- Test/BMIStrategyTest.java
+++ Test/BMIStrategyTest.java
@@ -1,3 +1,24 @@
 package Test;
 
+import org.junit.jupiter.api.Test;
 
+import src.Exercise.BMIStrategy;
+import src.Exercise.Exercise;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BMIStrategyTest {
+
+    @Test
+    public void testUnderweightBmi() {
+        BMIStrategy strategy = new BMIStrategy();
+        List<Exercise> exercises = strategy.getExercises("1.70,50");
+        assertEquals(2, exercises.size());
+        assertEquals("Light Yoga", exercises.get(0).getName());
+        assertEquals("Walking", exercises.get(1).getName());
+    }
+
+
+}
