08c13f82c699f3ca2d1c9c05c80b39c820e6dfa2
era97 <afrinjahanera03@gmail.com>
2024-12-07 13:26:31 +0600

    modifyed Factory test

--- Test/ExerciseStrategyFactoryTest.java
+++ Test/ExerciseStrategyFactoryTest.java
@@ -26,6 +26,19 @@ public class ExerciseStrategyFactoryTest {
         assertTrue(strategy instanceof BodyPartStrategy, "Should return an instance of BodyPartStrategy.");
     }
 
+    @Test
+    public void testHealthConditionStrategy() {
+        ExerciseStrategyFactory factory = new ExerciseStrategyFactory();
+        ExerciseStrategy strategy = factory.getStrategy("healthcondition");
+        assertTrue(strategy instanceof HealthConditionStrategy, "Should return an instance of HealthConditionStrategy.");
+    }
+
+    @Test
+    public void testInvalidStrategy() {
+        ExerciseStrategyFactory factory = new ExerciseStrategyFactory();
+        ExerciseStrategy strategy = factory.getStrategy("invalid");
+        assertNull(strategy, "Invalid strategy key should return null.");
+    }
 
     private void assertNotNull(ExerciseStrategy strategy, String bmiStrategy_should_not_be_null) {
         throw new UnsupportedOperationException("Not supported yet.");
