56a2c7ba52284dad6f383a1c7370d28c8c718fa9
era97 <afrinjahanera03@gmail.com>
2024-12-07 13:16:52 +0600

    testDefaultStretching

--- Test/BodyPartStrategyTest.java
+++ Test/BodyPartStrategyTest.java
@@ -19,4 +19,21 @@ public class BodyPartStrategyTest {
         assertEquals("Squats", exercises.get(0).getName());
         assertEquals("Lunges", exercises.get(1).getName());
     }
+    
+    @Test
+    public void testArmExercises() {
+        BodyPartStrategy strategy = new BodyPartStrategy();
+        List<Exercise> exercises = strategy.getExercises("arm");
+        assertEquals(2, exercises.size());
+        assertEquals("Bicep Curls", exercises.get(0).getName());
+        assertEquals("Push Ups", exercises.get(1).getName());
+    }
+
+    @Test
+    public void testDefaultStretching() {
+        BodyPartStrategy strategy = new BodyPartStrategy();
+        List<Exercise> exercises = strategy.getExercises("head");
+        assertEquals(1, exercises.size());
+        assertEquals("Stretching", exercises.get(0).getName());
+    }
 }
