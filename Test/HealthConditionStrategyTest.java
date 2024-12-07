2d3f19c31bd4e5338188065c1f132f76fbef1013
era97 <afrinjahanera03@gmail.com>
2024-12-07 13:17:51 +0600

    HealthCondition Test

--- Test/HealthConditionStrategyTest.java
+++ Test/HealthConditionStrategyTest.java
@@ -1,2 +1,21 @@
 package Test;
 
+import org.junit.jupiter.api.Test;
+
+import src.Exercise.Exercise;
+import src.Exercise.HealthConditionStrategy;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HealthConditionStrategyTest {
+
+    @Test
+    public void testBackCondition() {
+        HealthConditionStrategy strategy = new HealthConditionStrategy();
+        List<Exercise> exercises = strategy.getExercises("back");
+        assertEquals(1, exercises.size());
+        assertEquals("Back Stretches", exercises.get(0).getName());
+    }
+}
